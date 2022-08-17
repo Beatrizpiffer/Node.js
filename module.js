f7863ce4c679d542813d2645db3d25b07684168b
BEATRIZ PIFFER SILVA <81167411+Beatrizpiffer@users.noreply.github.com>
2022-08-17 10:58:44 -0300

    Create module.js

---
+++ module.js
@@ -0,0 +1,23 @@
+var soma = function(a,b)
+return a+b;{
+}
+
+module.exports = soma;
+
+var sub = function(a,b)
+return a-b;{
+}
+
+module.export = sub;
+
+var multi = function(a,b)
+return a*b;{
+}
+
+module.export = multi;
+
+var div = function(a,b)
+return a/b;{
+}
+
+module.export = div;
