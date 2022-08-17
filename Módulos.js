a717ef1c7b3b1b6266822c49a36f1284dc544635
BEATRIZ PIFFER SILVA <81167411+Beatrizpiffer@users.noreply.github.com>
2022-08-17 10:52:59 -0300

    Create Módulos.js

---
+++ Módulos.js
@@ -0,0 +1,17 @@
+function soma(a,b) {
+return a+b;
+}
+
+function sub(a,b) {
+return a-b;
+}
+
+function multi(a,b) {
+return a*b;
+}
+
+function div(a,b) {
+return a/b;
+}
+
+console.log(sub(2,4));
